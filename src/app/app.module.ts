28c996bf028ecfaf256fbe7bb1f83c24f3b91159
suma <sumazcode@gmail.com>
2023-04-30 14:07:57 +0530

    Added navigation in header, search component. CSS styles without bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { HeaderComponent } from './components/header/header.component';
+
 import { ProductService } from './services/product.service';
+import { FooterComponent } from './components/footer/footer.component';
+import { SearchbarComponent } from './components/searchbar/searchbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    HeaderComponent,
+    FooterComponent,
+    SearchbarComponent
   ],
   imports: [
     BrowserModule,
